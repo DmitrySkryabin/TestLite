5cb112116530d1c02e272667fbe4d8d6360e5ca1
Dmitry Skryabin <skryabin_ds@albank.ru>
2023-08-22 18:01:24 +0900

    refactor create view

--- TestLite/tests/models.py
+++ TestLite/tests/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+def invalid_name_validator(value):
+    if 'invalid' in value:
+        raise ValidationError('Это тестовое не валидируемое название')
+    else:
+        return value
 
 
 class Test(models.Model):
@@ -12,18 +20,17 @@ class Test(models.Model):
         MEDIUM = 'Средний'
         HIGH = 'Высокий'
 
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=200, validators=[invalid_name_validator])
+    description = models.TextField(validators=[invalid_name_validator])
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     priority = models.CharField(choices=PriorityChoice.choices, max_length=10)
 
-    #created_on = models.DateField(default=timezone.now)
     created_on = models.DateField(auto_now=True)
     modified_on = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('tests:test_detail', kwargs={'id': self.id})
     
@@ -31,8 +38,8 @@ class Test(models.Model):
 
 class TestPrecondition(models.Model):
     '''Предусловия для теста'''
-    action = models.CharField(max_length=500) # Может больше или меньше
-    expected_result = models.TextField()
+    action = models.CharField(max_length=500, validators=[invalid_name_validator]) # Может больше или меньше
+    expected_result = models.TextField(validators=[invalid_name_validator])
     position = models.IntegerField()
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
@@ -46,10 +53,10 @@ class TestPrecondition(models.Model):
 
 class TestStep(models.Model):
     '''Шаги которые включены в тест'''
-    action = models.CharField(max_length=500) # Может больше или меньше
-    expected_result = models.TextField()
+    action = models.CharField(max_length=500, validators=[invalid_name_validator]) # Может больше или меньше
+    expected_result = models.TextField(validators=[invalid_name_validator])
     position = models.IntegerField()
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, blank=True)
 
     class Meta:
         ordering = ['position']
@@ -62,8 +69,8 @@ class TestStep(models.Model):
 
 class TestPostcondition(models.Model):
     '''Постусловия для теста'''
-    action = models.CharField(max_length=500) # Может больше или меньше
-    expected_result = models.TextField()
+    action = models.CharField(max_length=500, validators=[invalid_name_validator]) # Может больше или меньше
+    expected_result = models.TextField(validators=[invalid_name_validator])
     position = models.IntegerField()
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
--- TestLite/tests/views.py
+++ TestLite/tests/views.py
@@ -35,30 +35,30 @@ class TestSaveUpdateMixin():
 
 
 
-class TestUpdateView(UpdateView):
-    '''Редактирование инстанса теста'''
-    template_name = 'tests/test_form.html'
-    form_class = TestForm
+# class TestUpdateView(UpdateView):
+#     '''Редактирование инстанса теста'''
+#     template_name = 'tests/test_form.html'
+#     form_class = TestForm
 
-    def get_object(self):
-        test_id = self.kwargs.get('id')
-        self.data = TestServices.get_all_test_attributes(id=test_id)
+#     def get_object(self):
+#         test_id = self.kwargs.get('id')
+#         self.data = TestServices.get_all_test_attributes(id=test_id)
 
-        return self.data.test
+#         return self.data.test
 
 
-    def get_context_data(self):
-        context = super().get_context_data()
-        context.update(
-            TestServices.get_tests_all_forms(self.data)
-        )
+#     def get_context_data(self):
+#         context = super().get_context_data()
+#         context.update(
+#             TestServices.get_tests_all_forms(self.data)
+#         )
 
-        return context
+#         return context
         
 
-    def form_valid(self, form):
-        TestServices.save_test(form=form, request=self.request)
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         TestServices.save_test(form=form, request=self.request)
+#         return super().form_valid(form)
 
 
 
@@ -105,85 +105,227 @@ class TestUpdateView(UpdateView):
 #         return redirect(reverse('tests:test_detail', kwargs={'id': test.id}))
 
 
+# from .forms import TestPreconditionForm, TestStepForm, TestPostconditionForm
+# from .models import Test, TestPrecondition, TestStep, TestPostcondition
+# from django.forms import modelformset_factory
+# class TestCreateView(CreateView):
+
+#     template_name = 'tests/test_form.html'
+#     form_class = TestForm
+
+
+#     def __init__(self):
+#         self.form_precondition_formset = modelformset_factory(model=TestPrecondition, form=TestPreconditionForm)
+#         self.form_step_formset = modelformset_factory(model=TestStep, form=TestStepForm)
+#         self.form_postcondition_formset = modelformset_factory(model=TestPostcondition, form=TestPostconditionForm)
+
+
+#     def get_context_data(self, **kwargs: Any):
+#         print('------------------GET CONTEXT DATA--------------------------')
+        
+#         #context = super().get_context_data(**kwargs)
+#         context = {}
+#         if kwargs.get('test_form') is not None:
+#             context['form'] = kwargs.get('test_form')
+#         else:
+#             context['form'] = TestForm()
+#         if kwargs.get('form_precondition_formset') is not None:
+#             print('RELOAD')
+#             context['form_precondition_formset'] =  kwargs.get('form_precondition_formset')
+#         else:
+#             print('NEW')
+             
+#             context['form_precondition_formset'] = self.form_precondition_formset(queryset=TestPrecondition.objects.none())
+#         if kwargs.get('form_step_formset') is not None:
+#             context['form_step_formset'] = kwargs.get('form_step_formset')
+#         else:
+            
+#             context['form_step_formset'] = self.form_step_formset(queryset=TestStep.objects.none())
+#         if kwargs.get('form_postcondition_formset') is not None:
+#             context['form_postcondition_formset'] = kwargs.get('form_postcondition_formset')
+#         else:
+            
+#             context['form_postcondition_formset'] = self.form_postcondition_formset(queryset=TestPostcondition.objects.none())
+
+#         return context
+    
+
+#     def post(self, request):
+#         print('------------------POST--------------------------')
+#         test_form = TestForm(request.POST)
+#         form_precondition_formset = self.form_precondition_formset(request.POST)
+#         form_step_formset = self.form_step_formset(request.POST)
+#         form_postcondition_formset = self.form_postcondition_formset(request.POST)
+
+#         if test_form.is_valid() and form_precondition_formset.is_valid() and form_step_formset.is_valid() and form_postcondition_formset.is_valid():
+#             return self.form_valid(
+#                 test_form=test_form,
+#                 form_precondition_formset=form_precondition_formset,
+#                 form_step_formset=form_step_formset,
+#                 form_postcondition_formset=form_postcondition_formset
+#             )
+#         else:
+#             return self.form_invalid(
+#                 test_form=test_form,
+#                 form_precondition_formset=form_precondition_formset,
+#                 form_step_formset=form_step_formset,
+#                 form_postcondition_formset=form_postcondition_formset
+#             )
+    
+
+#     def form_valid(self, test_form, form_precondition_formset, form_step_formset, form_postcondition_formset):
+#         print('------------------FORM VALID--------------------------')
+#         test = test_form.instance
+#         test.author = self.request.user
+#         test.save()
+#         test_precondtions = form_precondition_formset.save(commit=False)
+#         #test_precondtions.test = test
+#         for item in test_precondtions:
+#             print(item)
+#         # for instance in form_precondition_formset.save(commit=False):
+#         #     instance.save()
+#         # for instance in form_step_formset.save(commit=False):
+#         #     instance.save()
+#         # for instance in form_postcondition_formset.save(commit=False):
+#         #     instance.save()
+
+#         return redirect(reverse('tests:test_detail', kwargs={'id': test.id}))
+    
+
+#     def form_invalid(self, test_form, form_precondition_formset, form_step_formset, form_postcondition_formset):
+#         print('------------------FORM INVALID--------------------------')
+#         return self.render_to_response(self.get_context_data(
+#             test_form=test_form,
+#             form_precondition_formset=form_precondition_formset,
+#             form_step_formset=form_step_formset,
+#             form_postcondition_formset=form_postcondition_formset
+#         ))
+
+
 from .forms import TestPreconditionForm, TestStepForm, TestPostconditionForm
+from .models import Test, TestPrecondition, TestStep, TestPostcondition
+from django.forms import modelformset_factory
 class TestCreateView(CreateView):
 
-    template_name = 'tests/test_form.html'
+    model=Test
+    form_class=TestForm
+
+    def get_context_data(self, **kwargs):
+        print('==========GET CONTEXT DATA============')
+        context = super().get_context_data(**kwargs)
+        form_precondition_formset = modelformset_factory(form=TestPreconditionForm, model=TestPrecondition)
+        form_step_formset = modelformset_factory(form=TestStepForm, model=TestStep)
+        form_postcondition_formset = modelformset_factory(form=TestPostconditionForm, model=TestPostcondition)
+
+        if self.request.POST:
+            print('----------POST--------------')
+            context['form_precondition_formset'] = form_precondition_formset(self.request.POST, prefix='preconditions')
+            context['form_step_formset'] = form_step_formset(self.request.POST, prefix='steps')
+            context['form_postcondition_formset'] = form_postcondition_formset(self.request.POST, prefix='postconditions')
+        else:
+            print('-----------GET----------')
+            context['form_precondition_formset'] = form_precondition_formset(queryset=TestPrecondition.objects.none(), prefix='preconditions')
+            context['form_step_formset'] = form_step_formset(queryset=TestStep.objects.none(), prefix='steps')
+            context['form_postcondition_formset'] = form_postcondition_formset(queryset=TestPostcondition.objects.none(), prefix='postconditions')
+
+        print(context['form_precondition_formset'])                   
+        print(context['form_step_formset'])
+        print(context['form_postcondition_formset'])
+        return context
+
+
+    def form_valid(self, form):
+        print('===========FORM VALID============')
+        context = self.get_context_data()
+        if form.is_valid():
+            print('------FORM VALID------')
+            if context['form_precondition_formset'].is_valid() and context['form_step_formset'].is_valid() and context['form_postcondition_formset'].is_valid():
+                print('------FORMSETS IS VALID--------')
+                test = form.save(commit=False)
+                test.author = self.request.user
+                test.save()
+                for instance in context['form_precondition_formset'].save(commit=False):
+                    instance.test = test
+                    instance.save()
+                for instance in context['form_step_formset'].save(commit=False):
+                    instance.test = test
+                    instance.save()
+                for instance in context['form_postcondition_formset'].save(commit=False):
+                    instance.test = test
+                    instance.save()
+                return redirect(reverse('tests:test_detail', kwargs={'id': test.id}))
+            else:  
+                print('--------FORMSET INVALID----------')
+                print(context['form_precondition_formset'])                   
+                print(context['form_step_formset'])
+                print(context['form_postcondition_formset'])
+                print('\n')
+        else:
+            print('------FORM INVALID-------')
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+    # def form_invalid(self, form):
+    #     print('=======FORM INVALID=======')
+    #     return self.render_to_response(self.get_context_data(form=form))
+
+
+class TestUpdateView(UpdateView):
+
+    model = Test
     form_class = TestForm
 
 
-    def get_context_data(self, **kwargs: Any):
-        print('------------------GET CONTEXT DATA--------------------------')
-        #context = super().get_context_data(**kwargs)
-        context = {}
-        if kwargs.get('test_form') is not None:
-            context['form'] = kwargs.get('test_form')
-        else:
-            context['form'] = TestForm()
-        if kwargs.get('form_precondition_formset') is not None:
-            print('RELOAD')
-            context['form_precondition_formset'] =  kwargs.get('form_precondition_formset')
-        else:
-            print('NEW')
-            context['form_precondition_formset'] = TestPreconditionForm()
-        if kwargs.get('form_step_formset') is not None:
-            context['form_step_formset'] = kwargs.get('form_step_formset')
-        else:
-            context['form_step_formset'] = TestStepForm()
-        if kwargs.get('form_postcondition_formset') is not None:
-            context['form_postcondition_formset'] = kwargs.get('form_postcondition_formset')
-        else:
-            context['form_postcondition_formset'] = TestPostconditionForm()
+    def get_context_data(self, **kwargs):
+        print('==========GET CONTEXT DATA============')
+        context = super().get_context_data(**kwargs)
+        form_precondition_formset = modelformset_factory(form=TestPreconditionForm, model=TestPrecondition)
+        form_step_formset = modelformset_factory(form=TestStepForm, model=TestStep)
+        form_postcondition_formset = modelformset_factory(form=TestPostconditionForm, model=TestPostcondition)
 
+        if self.request.POST:
+            print('----------POST--------------')
+            context['form_precondition_formset'] = form_precondition_formset(self.request.POST, prefix='preconditions')
+            context['form_step_formset'] = form_step_formset(self.request.POST, prefix='steps')
+            context['form_postcondition_formset'] = form_postcondition_formset(self.request.POST, prefix='postconditions')
+        else:
+            print('-----------GET----------')
+            context['form_precondition_formset'] = form_precondition_formset(queryset=TestPrecondition.objects.filter(id=self.kwargs.get('id')), prefix='preconditions')
+            context['form_step_formset'] = form_step_formset(queryset=TestStep.objects.filter(id=self.kwargs.get('id')), prefix='steps')
+            context['form_postcondition_formset'] = form_postcondition_formset(queryset=TestPostcondition.objects.filter(id=self.kwargs.get('id')), prefix='postconditions')
+
+        print(context['form_precondition_formset'])                   
+        print(context['form_step_formset'])
+        print(context['form_postcondition_formset'])
         return context
-    
 
-    def post(self, request):
-        print('------------------POST--------------------------')
-        test_form = TestForm(request.POST)
-        form_precondition_formset = TestPreconditionForm(request.POST)
-        form_step_formset = TestStepForm(request.POST)
-        form_postcondition_formset = TestPostconditionForm(request.POST)
 
-        if test_form.is_valid() and form_precondition_formset.is_valid() and form_step_formset.is_valid() and form_postcondition_formset.is_valid():
-            return self.form_valid(
-                test_form=test_form,
-                form_precondition_formset=form_precondition_formset,
-                form_step_formset=form_step_formset,
-                form_postcondition_formset=form_postcondition_formset
-            )
+    def form_valid(self, form):
+        print('===========FORM VALID============')
+        context = self.get_context_data()
+        if form.is_valid():
+            print('------FORM VALID------')
+            if context['form_precondition_formset'].is_valid() and context['form_step_formset'].is_valid() and context['form_postcondition_formset'].is_valid():
+                print('------FORMSETS IS VALID--------')
+                test = form.save(commit=False)
+                test.author = self.request.user
+                test.save()
+                for instance in context['form_precondition_formset'].save(commit=False):
+                    instance.test = test
+                    instance.save()
+                for instance in context['form_step_formset'].save(commit=False):
+                    instance.test = test
+                    instance.save()
+                for instance in context['form_postcondition_formset'].save(commit=False):
+                    instance.test = test
+                    instance.save()
+                return redirect(reverse('tests:test_detail', kwargs={'id': test.id}))
+            else:  
+                print('--------FORMSET INVALID----------')
+                print(context['form_precondition_formset'])                   
+                print(context['form_step_formset'])
+                print(context['form_postcondition_formset'])
+                print('\n')
         else:
-            return self.form_invalid(
-                test_form=test_form,
-                form_precondition_formset=form_precondition_formset,
-                form_step_formset=form_step_formset,
-                form_postcondition_formset=form_postcondition_formset
-            )
-    
-
-    def form_valid(self, test_form, form_precondition_formset, form_step_formset, form_postcondition_formset):
-        print('------------------FORM VALID--------------------------')
-        test = test_form.instance
-        test.author = self.request.user
-        test.save()
-        test_precondtions = form_precondition_formset.save(commit=False)
-        test_precondtions.test = test
-        print(test_precondtions)
-        # for instance in form_precondition_formset.save(commit=False):
-        #     instance.save()
-        # for instance in form_step_formset.save(commit=False):
-        #     instance.save()
-        # for instance in form_postcondition_formset.save(commit=False):
-        #     instance.save()
-
-        return redirect(reverse('tests:test_detail', kwargs={'id': test.id}))
-    
-
-    def form_invalid(self, test_form, form_precondition_formset, form_step_formset, form_postcondition_formset):
-        print('------------------FORM INVALID--------------------------')
-        return self.render_to_response(self.get_context_data(
-            test_form=test_form,
-            form_precondition_formset=form_precondition_formset,
-            form_step_formset=form_step_formset,
-            form_postcondition_formset=form_postcondition_formset
-        ))
+            print('------FORM INVALID-------')
+        return self.render_to_response(self.get_context_data(form=form))
